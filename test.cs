0e921b8dfe6cfaef34ee112983931a843cb75f07
Miguel <>
2023-02-11 17:15:41 -0500

    Added Basic I/O Prompt/ PIN-420

--- test.cs
+++ test.cs
@@ -14,5 +14,8 @@ namespace HelloWorld
             System.Console.WriteLine(i);
         }
             
+            System.Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
     }
 }
