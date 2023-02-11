f062c9ad9de5c49bd4f586d38d57e936928fcd6b
Miguel <>
2023-02-11 13:19:26 -0500

    Basic Loop in C# Test

--- test.cs
+++ test.cs
@@ -7,5 +7,12 @@ namespace HelloWorld
             System.Console.WriteLine("Hello World!");
             System.Console.WriteLine("Testing Branching!");
         }
+
+        //Basic loop in C#
+        int test = 4;
+        while(int i = 0; i < test; i++){
+            System.Console.WriteLine(i);
+        }
+            
     }
 }
